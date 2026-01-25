412c58a62190bccc13b7b8173642cbc461b4ba55
Asstrid98 <taylinnzambrano@gmail.com>
2026-01-25 22:03:01 +0100

    Fase 1.

--- app/app/app/utils.py
+++
@@ -1,7 +0,0 @@
-import string
-import random
-
-def generate_short_code(length=6):
-    """Genera un código corto aleatorio."""
-    characters = string.ascii_letters + string.digits
-    return ''.join(random.choice(characters) for _ in range(length))
